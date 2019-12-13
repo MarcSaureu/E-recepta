9a9181e77c27536e781efcc203ff2501d5b58531
marc saureu <marcsaureu@gmail.com>
2019-12-13 12:22:49 +0100

    Creation Methods for class Sale and Implementation of some of methods

--- src/pharmacy/Sale.java
+++ src/pharmacy/Sale.java
@@ -1,4 +1,52 @@
 package pharmacy;
 
+import data.PatientContr;
+import data.ProductID;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Sale {
+    private int saleCode;
+    private Date date;
+    private BigDecimal amount;
+    private boolean isClosed;
+
+    public Sale(int sale, Date dat, BigDecimal big){
+        this.saleCode = sale;
+        this.date = dat;
+        this.amount = big;
+    }
+    public void addLine(ProductID prodID, BigDecimal price, PatientContr contr) throws SaleClosedException{
+
+    }
+    private void calculateAmount(){
+
+    }
+    private void addTaxes() throws SaleClosedException{
+        BigDecimal amount = this.amount;
+        amount.multiply(new BigDecimal(0.21));
+        this.amount.add(amount);
+    }
+    public void CalculateFinalAmount() throws SaleClosedException{
+
+    }
+    public BigDecimal getAmount(){
+        addTaxes();
+        return this.amount;
+    }
+    private void setClosed(){
+        this.isClosed = true;
+    }
+    public boolean isClosed(){
+        return this.isClosed;
+    }
+
+    public int getSaleCode() {
+        return this.saleCode;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
 }
