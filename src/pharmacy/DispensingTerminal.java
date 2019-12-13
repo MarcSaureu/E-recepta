a0cacb5798ab6b3060abdd0b1b0ca69dce2604db
marc saureu <marcsaureu@gmail.com>
2019-12-13 12:21:54 +0100

    Creation Methods for class DispensingTerminal

--- src/pharmacy/DispensingTerminal.java
+++ src/pharmacy/DispensingTerminal.java
@@ -1,4 +1,32 @@
 package pharmacy;
 
+import data.ProductID;
+
+import java.math.BigDecimal;
+
 public class DispensingTerminal {
+    public DispensingTerminal(){
+
+    }
+    public void getePrenscription(char option){
+
+    }
+    public void initNewSale(){
+
+    }
+    public void enterProduct(ProductID pID){
+
+    }
+    public void finalizeSale(){
+
+    }
+    public void realizePayment(BigDecimal quantity){
+
+    }
+    public void realizePayment(){
+
+    }
+    public void printNextDispensingSheet(){
+
+    }
 }
