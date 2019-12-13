6200245b2e8c90b468463ba1259fd7ad79dd44f5
marc saureu <marcsaureu@gmail.com>
2019-12-13 12:21:30 +0100

    Creation Methods for class Dispensing

--- src/pharmacy/Dispensing.java
+++ src/pharmacy/Dispensing.java
@@ -1,4 +1,24 @@
 package pharmacy;
 
+import data.ProductID;
+
+import java.util.Date;
+
 public class Dispensing {
+    private byte nOrder;
+    private Date initDate, finalDate;
+    private boolean isCompleted;
+
+    public Dispensing(){
+
+    }
+    public boolean dispensingEnabled() throws DispensingNotAvaiableException{
+       return false;
+    }
+    public void setProductAsDispensed(ProductID prodID){
+
+    }
+    public void setCompleted(boolean comp){
+        this.isCompleted = comp;
+    }
 }
