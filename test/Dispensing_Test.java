cbc6abd331b4f9e62a91f05b2a4c00088ef6f327
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2019-12-26 21:11:49 +0100

    Dispensing Test Creation

---
+++ test/Dispensing_Test.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import pharmacy.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class Dispensing_Test {
+    private byte order;
+    private Dispensing dispensing = new Dispensing(order,new Date(), new Date());
+
+}
