66027c25ae9f30564d17ad71f03e02d80960cbf1
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2019-12-26 21:34:58 +0100

    Dispensing Test Modification

--- test/Dispensing_Test.java
+++ test/Dispensing_Test.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-public class Dispensing_Test {
-    private byte order;
-    private Dispensing dispensing = new Dispensing(order,new Date(), new Date());
+class Dispensing_Test {
+    private byte order = 20;
+    private pharmacy.Dispensing dispensing = new Dispensing(order,new Date(), new Date());
+
+    @Test
+    void getOrder(){ assertEquals(20, dispensing.getnOrder());}
+
 
 }
