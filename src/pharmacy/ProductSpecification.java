ef9f6cecdb324e7d860b80b4819b125ccb036187
Marc Saureu <marcsaureu@gmail.com>
2019-12-26 19:42:18 +0100

    Class ProductSpecification Modified

--- src/pharmacy/ProductSpecification.java
+++ src/pharmacy/ProductSpecification.java
@@ -1,4 +1,18 @@
 package pharmacy;
 
+import data.ProductID;
+
+import java.math.BigDecimal;
+
 public class ProductSpecification {
+    private ProductID UPCcode;
+    private String description;
+    private BigDecimal price;
+
+    public String getDescription(){
+        return this.description;
+    }
+    public BigDecimal getPrice(){
+        return this.price;
+    }
 }
