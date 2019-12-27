f4d7d82b15982e94cd76d903107174b9068d7222
ahmedazzamouri <ahmedazzamouri98@gmail.com>
2019-12-27 02:11:37 +0100

    GetInitDate and GetEndDate Test Creation

--- test/Dispensing_Test.java
+++ test/Dispensing_Test.java
@@ -6,10 +6,17 @@ import java.util.Date;
 
 class Dispensing_Test {
     private byte order = 20;
-    private pharmacy.Dispensing dispensing = new Dispensing(order,new Date(), new Date());
+    private pharmacy.Dispensing dispensing = new Dispensing(order,new Date("2019/05/14"), new Date("2019/09/20"));
 
     @Test
     void getOrder(){ assertEquals(20, dispensing.getnOrder());}
 
+    @Test
+    void getInitDate(){ assertEquals(new Date("2019/05/14"), dispensing.getInitDate());
+    }
+    @Test
+    void getEndDate(){ assertEquals( new Date("2019/09/20"), dispensing.getFinalDate());
+
+    }
 
 }
